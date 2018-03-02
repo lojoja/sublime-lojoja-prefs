2f8c47723f2b34e1f6a40deef52ec9779f7e0121
lojoja <github@lojoja.com>
2018-03-02 16:29:39 -0500

    Move packages to settings files and always run on load
    This allows packages to be added/removed with a restart or reload of the plugin, without requiring a new version for every individual package change.

--- lojoja_prefs.py
+++ lojoja_prefs.py
@@ -1,27 +1,63 @@
 import sublime
 
 PACKAGE_NAME = '~lojojaPrefs'
-LP_SETTINGS_FILE = '{}.sublime-settings'.format(PACKAGE_NAME)
-PC_SETTINGS_FILE = 'Package Control.sublime-settings'
+SETTINGS_FILE = '{}.sublime-settings'.format(PACKAGE_NAME)
+PACKAGE_CONTROL_SETTINGS_FILE = 'Package Control.sublime-settings'
+SETTINGS_MAP = [
+    {
+        'current': 'install_prereleases',
+        'enabled': 'prerelease_packages',
+        'disabled': 'disabled_prerelease_packages'
+    },
+    {
+        'current': 'installed_packages',
+        'enabled': 'packages',
+        'disabled': 'disabled_packages'
+    }
+]
+
+
+def write_console(msg):
+    print('{}: {}'.format(PACKAGE_NAME, msg))
+
+
+def get_packages(current, enable, disable):
+    return sort_packages(set(current + enable).difference(disable))
+
+
+def sort_packages(packages):
+    return sorted(packages, key=str.lower)
 
 
 def plugin_loaded():
-    """ Force an update of the Package Control `installed_packages` list. """
-    try:
-        from package_control import events
-    except ImportError:
-        print('[{}] Failed to import `package_control.events`. Aborting `installed_packages` updated.'.format(PACKAGE_NAME))
-        return
+    """ Actions to run when plugin is loaded. """
 
-    if events.install(PACKAGE_NAME) or events.post_upgrade(PACKAGE_NAME):
-        lp_settings = sublime.load_settings(LP_SETTINGS_FILE)
-        pc_settings = sublime.load_settings(PC_SETTINGS_FILE)
+    # Manage packages
+    write_console('Checking package lists')
 
-        i_pkgs = pc_settings.get('installed_packages', [])
-        u_pkgs = lp_settings.get('uninstalled_packages', [])
-        i_pkgs = [p for p in i_pkgs if p not in u_pkgs]
+    settings = sublime.load_settings(SETTINGS_FILE)
+    package_control_settings = sublime.load_settings(PACKAGE_CONTROL_SETTINGS_FILE)
 
-        pc_settings.set('installed_packages', i_pkgs)
-        sublime.save_settings(PC_SETTINGS_FILE)
+    DEBUG = settings.get('debug', False)
 
-        print('[{}] Installed Package list updated'.format(PACKAGE_NAME))
+    changed = False
+    for keys in SETTINGS_MAP:
+        current = package_control_settings.get(keys['current'], [])
+        if DEBUG:
+            write_console('Current "{0}" package list: {1}'.format(keys['current'], current))
+
+        updated = get_packages(current,
+                               settings.get(keys['enabled'], []),
+                               settings.get(keys['disabled'], []))
+        if DEBUG:
+            write_console('New "{0}" package list: {1}'.format(keys['current'], updated))
+
+        if sort_packages(current) != updated:
+            changed = True
+            package_control_settings.set(keys['current'], updated)
+            write_console('Updated "{}" package list'.format(keys['current']))
+        else:
+            write_console('"{}" package list already up to date'.format(keys['current']))
+
+    if changed:
+        sublime.save_settings(PACKAGE_CONTROL_SETTINGS_FILE)
